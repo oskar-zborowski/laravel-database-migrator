3a60440aa1745d6ed02af1213cc13e9e9f430ce3
Oskar <oskarzborowski@gmail.com>
2023-12-27 22:47:24 +0100

    Developing a LaravelDatabaseStructure class

--- src/LaravelDatabaseStructure/LaravelDatabaseStructure.php
+++ src/LaravelDatabaseStructure/LaravelDatabaseStructure.php
@@ -3,20 +3,40 @@
 namespace OskarZborowski\LaravelDatabaseMigrator\LaravelDatabaseStructure;
 
 use Exception;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 use OskarZborowski\LaravelDatabaseMigrator\LaravelDatabaseMapper\LaravelDatabaseMapper;
 
 class LaravelDatabaseStructure
 {
+    private array $allowedClasses = [DB::class, Model::class];
+
     public function __construct(
         private string $object,
         private array $mappers,
+        // TODO Flaga z informacją czy funkcja zapisująca jest zapisywaniem masowym
+        // private ?string $createMethod = null,
+        // private ?string $updateMethod = null,
+        // private ?string $deleteMethod = null,
+        // TODO Flaga z określeniem czy brak elementów ma usunąć elementy
+        // TODO Pole z nazwą kolumny po znalezieniu którego element zostanie usunięty
+        // TODO Pole z nazwą kolumny do miękkiego usuwania
+        private ?string $defaultId = null,
+        // private DB|Connection|QueryBuilder|EloquentBuilder|Collection|array|string|null $defaultQuery = null,
+        private ?array $defaultBindings = null,
+        private ?string $defaultConnectionName = null,
+        private ?string $defaultLocalKey = null,
     ) {
-        $this->checkMappers();
+        $this->checkIfObjectIsAllowedInstance();
+        $this->checkIfMappersAreAllowedInstances();
+        $this->checkIfDefaultConnectionIsCorrect();
     }
 
     public function setObject(string $object): void
     {
         $this->object = $object;
+
+        $this->checkIfObjectIsAllowedInstance();
     }
 
     public function getObject(): string
@@ -28,13 +48,13 @@ class LaravelDatabaseStructure
     {
         $this->mappers = $mappers;
 
-        $this->checkMappers();
+        $this->checkIfMappersAreAllowedInstances();
     }
 
     public function addMappers(array $mappers): void
     {
         $this->setMappers(array_merge($this->getMappers(), $mappers));
-        $this->checkMappers();
+        $this->checkIfMappersAreAllowedInstances();
     }
 
     public function getMappers(): array
@@ -42,7 +62,92 @@ class LaravelDatabaseStructure
         return $this->mappers;
     }
 
-    private function checkMappers(): bool
+    public function setDefaultId(string $defaultId = null): void
+    {
+        $this->defaultId = $defaultId;
+    }
+
+    public function getDefaultId(): ?string
+    {
+        return $this->defaultId;
+    }
+
+    public function clearDefaultId(): void
+    {
+        $this->setDefaultId();
+    }
+
+    public function setDefaultBindings(array $defaultBindings = null): void
+    {
+        $this->defaultBindings = $defaultBindings;
+    }
+
+    public function addDefaultBindings(array $defaultBindings): void
+    {
+        $this->setDefaultBindings(array_merge($this->getDefaultBindings() ?? [], $defaultBindings));
+    }
+
+    public function getDefaultBindings(): ?array
+    {
+        return $this->defaultBindings;
+    }
+
+    public function clearDefaultBindings(): void
+    {
+        $this->setDefaultBindings();
+    }
+
+    public function setDefaultConnectionName(string $defaultConnectionName = null): void
+    {
+        $this->defaultConnectionName = $defaultConnectionName;
+
+        $this->checkIfDefaultConnectionIsCorrect();
+    }
+
+    public function getDefaultConnectionName(): ?string
+    {
+        return $this->defaultConnectionName;
+    }
+
+    public function clearDefaultConnectionName(): void
+    {
+        $this->setDefaultConnectionName();
+    }
+
+    public function setDefaultLocalKey(string $defaultLocalKey = null): void
+    {
+        $this->defaultLocalKey = $defaultLocalKey;
+    }
+
+    public function getDefaultLocalKey(): ?string
+    {
+        return $this->defaultLocalKey;
+    }
+
+    public function clearDefaultLocalKey(): void
+    {
+        $this->setDefaultLocalKey();
+    }
+
+    public function getAllowedClasses(): array
+    {
+        return $this->allowedClasses;
+    }
+
+    private function checkIfObjectIsAllowedInstance(): bool
+    {
+        $object = $this->getObject();
+
+        foreach ($this->getAllowedClasses() as $allowedClass) {
+            if (new $object instanceof $allowedClass) {
+                return true;
+            }
+        }
+
+        throw new Exception('Do uzupełnienia');
+    }
+
+    private function checkIfMappersAreAllowedInstances(): bool
     {
         foreach ($this->getMappers() as $mapper) {
             if (! $mapper instanceof LaravelDatabaseMapper) {
@@ -51,5 +156,16 @@ class LaravelDatabaseStructure
         }
 
         return true;
+    }
+
+    private function checkIfDefaultConnectionIsCorrect(): bool
+    {
+        if (null === $this->getDefaultConnectionName() ||
+            DB::connection($this->getDefaultConnectionName())->getPdo()
+        ) {
+            return true;
+        }
+
+        throw new Exception('Do uzupełnienia');
     }
 }
